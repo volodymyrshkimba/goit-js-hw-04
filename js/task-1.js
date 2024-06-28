2ee04a51df6058617130fa38d01758e13174795a
Volodymyr Shkimba <shkimbavova@gmail.com>
2024-06-28 19:22:29 +0300

    first check

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,27 @@
-function makeTransaction(quantity, pricePerDroid) {
-	return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
+function isEnoughCapacity(products, containerSize) {
+
+	let productSum = 0;
+
+	for (const product in products) {
+		productSum += products[product];
+	}
+
+	return productSum <= containerSize;
+
 }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
